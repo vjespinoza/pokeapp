99dfa650be26328edc1395618ee16264d48a3f8e
vjespinoza <vjespinoza84@gmail.com>
2021-08-24 11:50:21 -0500

    refactor: fetch api on usePokedex

--- src/components/showCase/showCase.js
+++ src/components/showCase/showCase.js
@@ -1,28 +1,34 @@
 import PokeCard from "./../pokeCard/pokeCard";
 import { ShowCaseContainer, PokedexGrid } from "./showCase.elements";
 //Custom hook
-import useFetchApi from "../../hooks/useFetchApi";
 import usePokedex from "../../hooks/usePokedex";
 
 const ShowCase = () => {
-    const { getRequest, fetchData } = useFetchApi({
-        url: "https://pokeapi.co/api/v2/pokemon/",
-    });
-
-    const { pokeData } = usePokedex({ getRequest, fetchData });
+    const { pokeDetails, gotoNextPage, gotoPrevPage, loading } = usePokedex();
 
     return (
         <ShowCaseContainer>
-            <h1>Pokédex</h1>
-            <PokedexGrid>
-                {pokeData
-                    .sort((a, b) => a.id - b.id)
-                    .map((poke, i) => {
-                        return (
-                            <PokeCard key={`${poke.name}-${i}`} poke={poke} />
-                        );
-                    })}
-            </PokedexGrid>
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <>
+                    <h1>Pokédex</h1>
+                    <button onClick={gotoPrevPage}>Prev</button>
+                    <button onClick={gotoNextPage}>Next</button>
+                    <PokedexGrid>
+                        {pokeDetails
+                            .sort((a, b) => a.id - b.id)
+                            .map((poke, i) => {
+                                return (
+                                    <PokeCard
+                                        key={`${poke.name}-${i}`}
+                                        poke={poke}
+                                    />
+                                );
+                            })}
+                    </PokedexGrid>
+                </>
+            )}
         </ShowCaseContainer>
     );
 };
--- src/hooks/usePokedex.js
+++ src/hooks/usePokedex.js
@@ -1,30 +1,61 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
-const usePokedex = ({ getRequest, fetchData }) => {
-    const [pokeData, setPokeData] = useState([]);
-
-    const getRandomPokes = (count) => {
-        for (let i = 0; i < count; i++) {
-            getRequest(Math.floor(Math.random() * 897) + 1);
-        }
-    };
-
-    const getAllPokes = (count) => {
-        for (let i = 0; i < count; i++) {
-            getRequest(i + 1);
-        }
-    };
+const usePokedex = () => {
+    const [pokemon, setPokemon] = useState([]);
+    const [currentPokemon, setCurrentPokemon] = useState({});
+    const [pokeDetails, setPokeDetails] = useState([]);
+    const [currentPageUrl, setCurrentPageUrl] = useState(
+        "https://pokeapi.co/api/v2/pokemon"
+    );
+    const [nextPageUrl, setNextPageUrl] = useState();
+    const [prevPageUrl, setPrevPageUrl] = useState();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        getAllPokes(40);
-        // getRandomPokes(12);
-    }, []);
+        setLoading(true);
+        let cancel;
+        axios
+            .get(currentPageUrl, {
+                cancelToken: new axios.CancelToken((c) => (cancel = c)),
+            })
+            .then((res) => {
+                setLoading(false);
+                setNextPageUrl(res.data.next);
+                setPrevPageUrl(res.data.previous);
+                setPokemon(res.data.results);
+            });
+
+        return () => cancel();
+    }, [currentPageUrl]);
 
     useEffect(() => {
-        fetchData && setPokeData([...pokeData, fetchData]);
-    }, [fetchData]);
+        pokemon.map((p) => {
+            return axios.get(p.url).then((res) => {
+                setCurrentPokemon(res.data);
+            });
+        });
+    }, [pokemon]);
 
-    return { pokeData };
+    useEffect(() => {
+        Object.entries(currentPokemon).length > 0 &&
+            setPokeDetails([...pokeDetails, currentPokemon]);
+    }, [currentPokemon]);
+
+    function gotoNextPage() {
+        setCurrentPageUrl(nextPageUrl);
+    }
+
+    function gotoPrevPage() {
+        setCurrentPageUrl(prevPageUrl);
+    }
+
+    return {
+        pokeDetails,
+        gotoNextPage,
+        gotoPrevPage,
+        loading,
+    };
 };
 
 export default usePokedex;
