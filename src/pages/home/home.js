a14ad270881265db26f8a8172889cbb9f25f361c
vjespinoza <vjespinoza84@gmail.com>
2021-08-25 17:15:26 -0500

    home page: temp button

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,17 @@
 import { PageContainer } from "../../components/shared/pageContainer";
+//Temp imports
+import { Link } from "react-router-dom";
+import { Button } from "../../components/shared/button";
 
 const Home = ({ user }) => {
     return (
         <PageContainer singlePage>
             <h1>Home page</h1>
             <h3>Bienvenido {user}</h3>
+            {/* Temp button */}
+            <Link to="pokedex">
+                <Button>Go to pokedex</Button>
+            </Link>
         </PageContainer>
     );
 };
